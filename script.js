90ab3d455b819bfaba41707e31c1ebef9ad82738
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2024-01-17 13:26:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 // complete this js code
-function Person(name, age) {
-	this.name = name;
-	this.age = age;
-}
-Person.prototype.greet = function(){
-	console.log(`Hello! my name is ${this.name}, I am ${this.age} year old`)
-}
-function Employee(name, age, jobTitle) {
-	Person.call(this, name, age);
-	this.jobTitle = jobTitle;
-}
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.jobGreet = function(){
-	console.log(`Hello! my name is ${this.name}, I am ${this.age} year old, my job Title is ${this.jobTitle}`);
-}
+// function Person(name, age) {
+// 	this.name = name;
+// 	this.age = age;
+// }
+// Person.prototype.greet = function(){
+// 	console.log(`Hello! my name is ${this.name}, I am ${this.age} year old`)
+// }
+// function Employee(name, age, jobTitle) {
+// 	Person.call(this, name, age);
+// 	this.jobTitle = jobTitle;
+// }
+// Employee.prototype = Object.create(Person.prototype);
+// Employee.prototype.jobGreet = function(){
+// 	console.log(`Hello! my name is ${this.name}, I am ${this.age} year old, my job Title is ${this.jobTitle}`);
+// }
 
 // Do not change code below this line
+// window.Person = Person;
+// window.Employee = Employee;
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+Person.prototype.greet = function () {
+    console.log(`Hello! My name is ${this.name}, I am ${this.age} years old`);
+};
+
+function Employee(name, age, jobTitle) {
+    Person.call(this, name, age);
+    this.jobTitle = jobTitle;
+}
+
+// Inherit the Person prototype into the Employee prototype
+Employee.prototype = Object.create(Person.prototype);
+
+Employee.prototype.jobGreet = function () {
+    console.log(`Hello! My name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
+};
+
+// Do not change the code below this line
 window.Person = Person;
 window.Employee = Employee;

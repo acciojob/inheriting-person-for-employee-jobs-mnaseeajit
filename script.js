ca8aea614444557152d7b56284b726560b4a6ae9
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2024-01-17 13:16:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function Employee(name, age, jobTitle) {
 	Person.call(this, name, age);
 	this.jobTitle = jobTitle;
 }
-
-Employee.prototype.jobGreet = functio(){
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.jobGreet = function(){
 	console.log(`Hello! my name is ${this.name}, I am ${this.age} year old, my job Title is ${this.jobTitle}`);
 }
 

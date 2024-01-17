87bc453faa0adbc14722f0b8db7e3b32cbb8755e
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2024-01-17 13:11:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+}
+Person.prototype.greet = function(){
+	console.log(`Hello! my name is ${this.name}, I am ${this.age} year old`)
+}
+function Employee(name, age, jobTitle) {
+	Person.call(this, name, age);
+	this.jobTitle = jobTitle;
+}
 
-function Employee(name, age, jobTitle) {}
+Employee.prototype.jobGreet = functio(){
+	console.log(`Hello! my name is ${this.name}, I am ${this.age} year old, my job Title is ${this.jobTitle}`);
+}
 
 // Do not change code below this line
 window.Person = Person;
